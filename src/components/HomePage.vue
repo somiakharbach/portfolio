a690e35b93806a74dec4541bd9126456f2533588
somia kharbach <kharbach2320@gmail.com>
2025-02-05 23:22:36 +0100

    update cv

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -11,7 +11,7 @@ function sendMail() {
 }
 
 function openCV() {
-  window.open('../src/assets/Kharbach-Somia-CV-1.pdf', '_blank');
+  window.open('/portfolio/src/assets/Kharbach-Somia-CV-1.pdf', '_blank');
 }
 
 </script>
